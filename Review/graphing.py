afd81eadd4338cc501089a71f07dd201f51f4fd5
mdwensma <mdwensma@ncsu.edu>
2025-01-28 09:43:22 -0500

    graphing

---
+++ Review/graphing.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4]
+y = [10, 15, 5, 12]
+
+# Create a line plot
+plt.plot(x, y)
+
+# Add title and labels
+plt.title("Simple Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Display the plot
+plt.show()
